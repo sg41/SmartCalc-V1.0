65d96fa13ae5ee0d23125ade557d300c1a8da216
joellebo <joellebo@student.21-school.ru>
2022-09-07 14:57:18 +0700

    interface first show

--- src/gtk.c
+++ src/gtk.c
@@ -1,12 +1,15 @@
 #include <gtk/gtk.h>
 
-static void on_button_clicked(GtkWidget *widget, gpointer data) {
+extern void button_clicked(GtkWidget *widget, gpointer data) {
   g_print("Hello World\n");
+}
+extern void exit_button_clicked(GtkWidget *widget, gpointer data) {
+  gtk_main_quit();
 }
 
 int main(int argc, char *argv[]) {
   GtkBuilder *builder;
-  GObject *window;
+  // GObject *window;
   GObject *button;
   GError *error = NULL;
 
@@ -14,14 +17,28 @@ int main(int argc, char *argv[]) {
 
   /* Construct a GtkBuilder instance and load our UI description */
   builder = gtk_builder_new();
-  if (gtk_builder_add_from_file(builder, "newversion.ui", &error) == 0) {
+  if (gtk_builder_add_from_file(builder, "newversion-v1.ui", &error) == 0) {
     g_printerr("Error loading file: %s\n", error->message);
     g_clear_error(&error);
     return 1;
   }
 
+  //Теперь получаем виджет из Builder
+  // Помните мы указывали ID? Вот по нему мы и ищем нужный
+  // В данном случае ищем виджет окна
+  GtkWidget *window =
+      GTK_WIDGET(gtk_builder_get_object(builder, "calc_window"));
+
+  //Таким же образом можно получить и другие виджеты
+  // но нам они не понадобятся
+
+  //Подключаем сигналы)
   gtk_builder_connect_signals(builder, NULL);
 
+  // Разрешаем отображение
+  gtk_widget_show_all(window);
+
+  //Пошла программа
   gtk_main();
 
   return 0;
