5abe2f8e92189d6e222929767b89fd45a6cef8ca
joellebo <joellebo@student.21-school.ru>
2022-09-10 11:16:28 +0700

    fist draw happend

--- src/gtk.c
+++ src/gtk.c
@@ -1,7 +1,57 @@
 #include "gtk.h"
 
+#include <cairo.h>
 #include <gtk/gtk.h>
 #include <string.h>
+
+#define WIDTH 640
+#define HEIGHT 480
+
+#define ZOOM_X 100.0
+#define ZOOM_Y 100.0
+
+gfloat f(gfloat x) { return 0.03 * pow(x, 3); }
+
+extern gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+  GdkRectangle da;            /* GtkDrawingArea size */
+  gdouble dx = 5.0, dy = 5.0; /* Pixels between each point */
+  gdouble i, clip_x1 = 0.0, clip_y1 = 0.0, clip_x2 = 0.0, clip_y2 = 0.0;
+  GdkWindow *window = gtk_widget_get_window(widget);
+
+  /* Determine GtkDrawingArea dimensions */
+  gdk_window_get_geometry(window, &da.x, &da.y, &da.width, &da.height);
+
+  /* Draw on a black background */
+  cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+  cairo_paint(cr);
+
+  /* Change the transformation matrix */
+  cairo_translate(cr, da.width / 2, da.height / 2);
+  cairo_scale(cr, ZOOM_X, -ZOOM_Y);
+
+  /* Determine the data points to calculate (ie. those in the clipping zone */
+  cairo_device_to_user_distance(cr, &dx, &dy);
+  cairo_clip_extents(cr, &clip_x1, &clip_y1, &clip_x2, &clip_y2);
+  // cairo_set_line_width(cr, dx);
+  cairo_set_line_width(cr, 0.01);
+
+  /* Draws x and y axis */
+  cairo_set_source_rgb(cr, 0.0, 0.0, 0.0);
+  cairo_move_to(cr, clip_x1, 0.0);
+  cairo_line_to(cr, clip_x2, 0.0);
+  cairo_move_to(cr, 0.0, clip_y1);
+  cairo_line_to(cr, 0.0, clip_y2);
+  cairo_stroke(cr);
+
+  /* Link each data point */
+  for (i = clip_x1; i < clip_x2; i += dx) cairo_line_to(cr, i, f(i));
+
+  /* Draw the curve */
+  cairo_set_source_rgba(cr, 1, 0.2, 0.2, 0.6);
+  cairo_stroke(cr);
+
+  return FALSE;
+}
 
 extern void func_button_clicked(GtkButton *button, gpointer data) {
   GtkEntry *src_str_entry = data;
@@ -217,7 +267,7 @@ int main(int argc, char *argv[]) {
 
   /* Construct a GtkBuilder instance and load our UI description */
   builder = gtk_builder_new();
-  if (gtk_builder_add_from_file(builder, "newversion-v2.ui", &error) == 0) {
+  if (gtk_builder_add_from_file(builder, "newversion-v3.ui", &error) == 0) {
     g_printerr("Error loading file: %s\n", error->message);
     g_clear_error(&error);
     return 1;
